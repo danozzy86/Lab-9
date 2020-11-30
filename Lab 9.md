9b796653a9da20f782c2fb905ea5d49960e2469d
danozzy86 <danozzy86@gmail.com>
2020-11-30 15:30:00 -0500

    DLO
    done, no bugs found yet

---
+++ Lab 9.md
@@ -0,0 +1,21 @@
+# Number Guessing
+
+Write a progarm that chooses a random number. The user then guess a number and the program say if the random number is lower or higher then the user guessed. The user input is sanitized for each input. 
+
+### Possiable points:
+* Full = 10
+* Partial = 5
+* None = 0
+
+### Due Date:
+11/23/2020 @ 4:00PM (Beginning of class)
+
+### How to upload:
+Zip all files into a compressed folder and upload to BB with the folder named "(LastName)Lab9.zip or upload a word document with a GitHub link included. If the link does not work I will email you and you will have 1 day (24 hours) of when I email you to provide a working link or send me a zip file matching the terms above.
+
+### Key points:
+* Program runs with out bugs.
+* Code is clean/read-able.
+* Code is commented.
+* Code is NOT copied from the internet.
+* All inputs from the user are sanitized.
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿//Daniel Osborne
+//Programming I - Lab 9 Number Guess
+//Github - danozzy86
+
+using System;
+using System.Threading;
+
+namespace Lab_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random =new Random(); //Creates a new instance of the random object.
+
+            Console.Clear();
+            Console.WriteLine("Enter a number greater than 0.");
+
+            int guessCount = 0; //Creates variable used to count the number of guesses.
+            int intMax = Sanitize(Console.ReadLine()); //Collects user input and sanitizes.
+            int intRand = random.Next(0, intMax); //Selects a random number within the designated range.
+
+            bool correct = false;//Loop Control
+
+            while(!correct){
+                //Will loop through until the number is guessed correctly, will state if the number is higher or lower with a guess counter.
+                Console.Clear();
+                Console.WriteLine("Try to guess the number between 0 and "+intMax+".");
+                Console.WriteLine("Total guesses = "+guessCount+".");
+
+                int guess = Sanitize(Console.ReadLine(), 0, intMax);
+
+                if(guess > intRand){
+                    guessCount += 1;
+                    
+                    Console.WriteLine("The number is lower than "+guess+".");
+                    Thread.Sleep(1500);
+                } else if(guess < intRand){
+                    guessCount += 1;
+                    
+                    Console.WriteLine("The number is greater than "+guess+".");
+                    Thread.Sleep(1500);
+                } else if(guess == intRand){
+                    guessCount += 1;
+
+                    Console.Clear();
+                    Console.WriteLine("You have guessed correctly, the number was "+guess+".");
+                    Console.WriteLine("Total number of guesses was "+guessCount+".");
+                    
+                    correct = true;
+                }
+            }
+        }
+
+        private static int Sanitize(string strUsrInput, int min = 0, int max = 2147483647){
+            //Sanitizes the user integer inputs to prevent errors. Limits the highest selectable number to the maximum possible 32bit integer by default.
+            bool valid = false;
+            int input = 0;
+
+            while(!valid){
+                if(!int.TryParse(strUsrInput, out int temp) || temp < min || temp > max){
+                    Console.WriteLine("Enter a valid integer input");
+                    strUsrInput = Console.ReadLine();
+                } else {
+                    input = temp;
+                    valid = true;
+                }
+            }
+            return input;
+        }
+    }
+}
